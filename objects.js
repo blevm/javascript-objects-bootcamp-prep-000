93f363a99c63c3fbf1075e1b349f84916031540e
Michelle B <mjblevins7@gmail.com>
2017-11-07 04:28:09 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = new Object({ The Beatles: "Hey Jude"};)
+
+function updatePlaylist (obj,artist,song) {
+Object.assign ({obj}, {artist,song});
+  return playlist;
+}
+
+function removeFromPlaylist (obj,artist) {
+Delete obj.artist;
+  return playlist;
+}
