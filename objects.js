05e7a23fe6419a2e9463138f1fc9c0242e12eae0
Michelle B <mjblevins7@gmail.com>
2017-11-07 04:34:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,10 +2,12 @@ var playlist = new Object();
 var playlist = new Object({ beatles: 'Hey Jude' })
 
 function updatePlaylist(playlist, artistName, songTitle) {
-return Object.assign ({playlist}, {artistName , songTitle});
+  return Object.assign({}, playlist, { [artistName]: songTitle })
 }
 
 function removeFromPlaylist(playlist, artistName) {
 Delete playlist.artistName;
   return playlist;
 }
+
+
